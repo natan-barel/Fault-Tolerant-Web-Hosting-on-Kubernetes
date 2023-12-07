d2259e82acad6308de21286b10cc663e9bef1f68
natan-barel <natan.barel@outlook.co.il>
2023-12-07 11:18:25 +0200

    added files

---
+++ Application/src/components/Projects/WorkCradData.js
@@ -0,0 +1,21 @@
+import pro1 from "../../assets/tf-project-1-architecture-diagram.png";
+import pro2 from "../../assets/prometheus-observability-stack-architecture-diagram.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "AWS - Terraform Project 1",
+    text: "Setup jenkins on Amazon-linux EC2 instance (under VPC) using Terraform and Ansible",
+    repo: "https://github.com/natan-barel/Terraform-Project-1/",
+    view: "",
+  },
+  {
+    imgsrc: pro2,
+    title: "AWS - Prometheus Observability Stack Using Docker",
+    text: "Prometheus, Grafana & Alert Manager using Terraform, Docker and Docker Compose",
+    repo: "https://github.com/natan-barel/prometheus-observability-stack",
+    view: "",
+  }
+];
+
+export default ProjectCardData;
